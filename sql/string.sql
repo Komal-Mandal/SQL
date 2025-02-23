070ca622d7379012ccd43ead087d934e42d583b8
Komal Mandal <komalmandal1907@gmail.com>
2025-02-23 23:39:38 +0530

    fourth commit

--- sql/string.sql
+++ sql/string.sql
@@ -6,3 +6,17 @@ select concat("hey" "buddy");
 
 select id,concat(NAME, " ", DESIGNATION) as fullname
 from employees;
+
+
+/* concat_ws means concat with separator */
+select concat_ws("-","hii","hello");
+
+select concat_ws(":","NAME","DESIGNATION","DEPT")
+FROM employees;
+
+/*  substring   */
+select substring("hello buddy",1,4);
+
+SELECT SUBSTRING(id,2) as emp_id,NAME from employees;
+
+

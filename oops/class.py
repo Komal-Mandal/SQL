429012f7c02ba1b471a2c413277ef7c4975b3539
Komal Mandal <komalmandal1907@gmail.com>
2025-03-15 00:42:37 +0530

    class

---
+++ oops/class.py
@@ -0,0 +1,21 @@
+class Atm:
+     """constructor = the code which is written inside the constructor runs automaticaly when object is created"""
+
+     # methode = function inside the class is called as methode 
+
+     def __init__(self):
+            self.pin = ""
+            self.balance = 0
+
+            self.menu()
+
+def menu(self):
+       user_input = """
+           hello,how would you like to proceed
+               
+
+
+
+"""
+      
+              
